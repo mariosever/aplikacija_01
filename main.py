4eb20b0fe6756a04f1d45a371e393c2fcc1e56c6
Mario Sever <34382956+mariosever@users.noreply.github.com>
2021-12-01 12:10:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Python Flask aplikacija
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/kontakt")
+def kontakt():
+    return render_template("kontakt.html")
+
+if __name__ == "__main__":
+    app.run(use_reloader=True)
